ad4efb03378967538d33df0f1f115bdb58c4c77b
d_yanis <dahmane.yanis.2004@gmail.com>
2024-01-10 17:22:28 +0100

    WIP

---
+++ Model/SuperHero.cs
@@ -0,0 +1,9 @@
+namespace Model;
+
+class SuperHero {
+    public string name {get; set;}
+
+    public SuperHero(string name) {
+        this.name = name;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Enigma.Components;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpClient();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
